99b37f74d4dd8a8641d960a5b8d9d7cd3f5cafe8
Satiel <mybeautifulwound@gmail.com>
2014-02-13 00:01:59 -0600

    First c# commit

---
+++ Scoundrel/Scoundrel/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // Map dimensions
+        public const int map_width = 20;
+        public const int map_height = 15;
+
+        // Tile Types
+        public const int tile_floor = 0;
+        public const int tile_wall = 1;
+
+        // Map declaration
+
+        public static int[,] mapArray = new int[15, 20]
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+	    };
+
+        // Function prototypes
+        public static void DrawMap()
+        {
+            // draw loop
+            for (int y = 0; y < map_height; y++)
+            {
+                // move down ond row
+                Console.Out.NewLine = "\r\n";
+                Console.WriteLine();
+
+                for (int x = 0; x < map_width; x++)
+                {
+                    // check the tile at that location
+                    switch (mapArray[y,x])
+                    {
+                        case 0:
+                            // draw a floor
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write('.');
+                            break;
+
+                        case 1:
+                            // draw a wall
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write('#');
+                            break;
+                    }
+                }
+            }
+
+            Console.SetCursorPosition(10, 10);
+            Console.Write('@');
+            Console.ReadKey();
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            DrawMap();
+        }
+    }
+}
