ebd2dff5216c2b3c12b1d14dc45d98a0813c7422
Satiel <mybeautifulwound@gmail.com>
2014-02-20 00:16:04 -0600

    added 'use item' command

--- Scoundrel/Scoundrel/Game.cs
+++ Scoundrel/Scoundrel/Game.cs
@@ -22,28 +22,31 @@ namespace ConsoleApplication1
 
         // Tile Types
         public const int TILE_FLOOR = 0;
-         const int TILE_WALL = 1;
-         const int TILE_TREE = 2;
-         const int TILE_CLOSED_DOOR = 3;
-         const int TILE_OPEN_DOOR = 4;
+        const int TILE_WALL = 1;
+        const int TILE_TREE = 2;
+        const int TILE_CLOSED_DOOR = 3;
+        const int TILE_OPEN_DOOR = 4;
+        const int TILE_LOCKED_DOOR = 5;
 
         // Item Types
-         const int ITEM_NONE = 0;
-         const int ITEM_POTION = 1;
-         const int ITEM_ROCK = 2;
+        const int ITEM_NONE = 0;
+        const int ITEM_POTION = 1;
+        const int ITEM_ROCK = 2;
+        const int ITEM_KEY = 3;
+        const int ITEM_AXE = 4;
 
         // Player Position
-         int playerX = 10;
-         int playerY = 10;
+        int playerX = 10;
+        int playerY = 10;
 
         // Player inventory
-         int[] inventory = new int[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-         const int INVENTORY_SLOTS = 10;
+        int[] inventory = new int[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        const int INVENTORY_SLOTS = 10;
 
         // Screen buffer variables
-         int[,] map; // array to store passed-in mapArray
-         List<TileType> listIndex = new List<TileType>(); // list to store TileTypes
-         List<ItemType> itemIndex = new List<ItemType>(); // list to store ItemTypes
+        int[,] map; // array to store passed-in mapArray
+        List<TileType> listIndex = new List<TileType>(); // list to store TileTypes
+        List<ItemType> itemIndex = new List<ItemType>(); // list to store ItemTypes
 
         // Map declaration
 
@@ -78,9 +81,9 @@ namespace ConsoleApplication1
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
             { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
@@ -111,11 +114,14 @@ namespace ConsoleApplication1
             listIndex.Add(new TileType('T', ConsoleColor.Green, false)); // tree
             listIndex.Add(new TileType('/', ConsoleColor.Magenta, false)); // closed door
             listIndex.Add(new TileType('_', ConsoleColor.Magenta, true)); // open door
+            listIndex.Add(new TileType('I', ConsoleColor.Magenta, false)); // locked door
 
             // Add ItemTypes to the ItemIndex
             itemIndex.Add(new ItemType(' ', ConsoleColor.Gray, "EMPTY")); // (0) ITEM_NONE (unused inventory slot)
             itemIndex.Add(new ItemType((char)176, ConsoleColor.Cyan, "Potion")); // (1) ITEM_POTION
             itemIndex.Add(new ItemType('*', ConsoleColor.DarkGray, "Rock")); // (2) ITEM_ROCK
+            itemIndex.Add(new ItemType((char)12, ConsoleColor.Yellow, "Key")); // (3) ITEM_KEY
+            itemIndex.Add(new ItemType('(', ConsoleColor.Red, "Pickaxe")); // (4) ITEM_AXE
 
             //set cursor position to top left and draw the string
             Console.SetCursorPosition(0, 0);
@@ -237,6 +243,8 @@ namespace ConsoleApplication1
 
         public void GetCommand()
         {
+
+             
            
             // First check to see if there's actually an item present underneath the player
             if (itemArray[playerY, playerX] == ITEM_NONE)
@@ -256,6 +264,9 @@ namespace ConsoleApplication1
                     // Move the item to the slot and remove it from the world
                     inventory[i] = itemArray[playerY, playerX];
                     itemArray[playerY, playerX] = ITEM_NONE;
+
+                    ClearText();
+
                     return;
                 }
             }
@@ -267,19 +278,9 @@ namespace ConsoleApplication1
 
         public void DropCommand()
         {
+            /** COMMANDS TO FIX INVENTORY TEXT ISSUES **/
 
-            Console.Clear();
-            DrawScreen(MAP_WIDTH, MAP_HEIGHT, mapArray);
-            // Draw the inventory
-            ShowInventory();
-
-            // Set the player's new position
-            Console.SetCursorPosition(playerX, playerY);
-
-            // Draw the player
-            Console.Write('@');
-
-
+            ClearText();
 
             char a = 'a';
             // Ask the user which inventory slot they're trying to drop
@@ -315,7 +316,9 @@ namespace ConsoleApplication1
 
             else if (itemArray[playerY, playerX] != ITEM_NONE)
             {
-
+                // There is already an item here
+                Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+                Console.WriteLine("There is already an item here");
             }
 
             else
@@ -327,6 +330,254 @@ namespace ConsoleApplication1
                 inventory[slot] = ITEM_NONE;
             }
 
+        }
+
+        public void UseItemCommand()
+        {
+            /** COMMANDS TO FIX INVENTORY TEXT ISSUES **/
+
+            ClearText();
+
+            char a = 'a';
+            // Ask the user which inventory slot they're trying to drop
+
+
+            // THIS IS A TEMPORARY FIX FOR THE ISSUE WHERE
+            // PLAYER INPUT GETS DISPLAYED ON THE SCREEN
+            // BY THE PLAYER ICON WHEN INITIALLY TYPING - FIND BETTER FIX FOR THIS
+            Console.CursorVisible = true;
+            Console.SetCursorPosition(2, MAP_HEIGHT);
+            Console.WriteLine("Use which item?");
+            var choice = Console.ReadKey();
+
+            // Convert the key press into an inventory slot
+            int slot = choice.KeyChar - (int)a;
+
+            // Verify that this is a valid slot
+            if (slot < 0 || slot >= INVENTORY_SLOTS)
+            {
+                // Complain to the user
+                Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+                Console.WriteLine("Invalid slot");
+            }
+
+            switch (inventory[slot])
+            {
+                // Empty inventory slot
+                case ITEM_NONE:
+                    // Complain to the user - nothing is here
+                    Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+                    Console.WriteLine("No item to use");
+
+
+                    break;
+
+                case ITEM_POTION:
+                    // Give a message to the user
+                    Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+                    Console.WriteLine("You drank the potion. *burp!*");
+
+                    // Remove the item from the inventory
+                    
+                    inventory[slot] = ITEM_NONE;
+                    ClearText();
+
+
+                    break;
+
+                case ITEM_AXE:
+                    // Use the pickaxe
+                    UsePickaxe();
+                    break;
+
+                case ITEM_KEY:
+                    // Use the key
+                    UseKey();
+                    break;
+
+                // We don't know what the item is
+                default:
+                    // Complain to the user
+                    Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+                    Console.WriteLine("Don't know how to use this item!");
+                    break;
+            }
+
+
+
+
+        }
+
+        public void UsePickaxe()
+        {
+
+            // Ask the user which direction he wants to axe in
+            Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+            Console.WriteLine("Use Pickaxe: which direction?");
+
+            // Get the user input for the direction
+            ConsoleKey key = Console.ReadKey().Key;
+            int deltaX = 0;
+            int deltaY = 0;
+
+            // Compute which tile he user specified
+            switch (key)
+            {
+                // SOUTH
+                case ConsoleKey.NumPad2:
+                    deltaX = 0;
+                    deltaY = 1;
+                    break;
+
+                // WEST
+                case ConsoleKey.NumPad4:
+                    deltaX = -1;
+                    deltaY = 0;
+                    break;
+                
+                // EAST
+                case ConsoleKey.NumPad6:
+                    deltaX = 1;
+                    deltaY = 0;
+                    break;
+
+                // NORTH
+                case ConsoleKey.NumPad8:
+                    deltaX = 0;
+                    deltaY = -1;
+                    break;
+
+                // Not a valid direction
+                default:
+                    // no direction specified, so abort
+                    ClearText();
+                    break;
+            }
+
+            if (mapArray[playerY + deltaY, playerX + deltaX] != TILE_WALL)
+            {
+                // Complain to the user
+                ClearText();
+                Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+                Console.WriteLine("Can't use that item here!");
+
+               
+                return;
+            }
+
+            // change the selected tile from a wall to a rock floor
+            mapArray[playerY + deltaY, playerX + deltaX] = TILE_FLOOR;
+
+            // place a rock there to simulate rubble
+            itemArray[playerY + deltaY, playerX + deltaX] = ITEM_ROCK;
+
+            Console.SetCursorPosition(2, MAP_HEIGHT + 4);
+            Console.WriteLine("You smash the stone to pieces.");
+        }
+
+        public void UseKey()
+        {
+            // Ask the user which direction he wants to axe in
+            Console.SetCursorPosition(2, MAP_HEIGHT + 3);
+            Console.WriteLine("Use key: which direction?");
+
+            // Get the user input for the direction
+            ConsoleKey key = Console.ReadKey().Key;
+            int deltaX = 0;
+            int deltaY = 0;
+
+            // Compute which tile he user specified
+            switch (key)
+            {
+                // SOUTH
+                case ConsoleKey.NumPad2:
+                    deltaX = 0;
+                    deltaY = 1;
+                    break;
+
+                // WEST
+                case ConsoleKey.NumPad4:
+                    deltaX = -1;
+                    deltaY = 0;
+                    break;
+
+                // EAST
+                case ConsoleKey.NumPad6:
+                    deltaX = 1;
+                    deltaY = 0;
+                    break;
+
+                // NORTH
+                case ConsoleKey.NumPad8:
+                    deltaX = 0;
+                    deltaY = -1;
+                    break;
+
+                // Not a valid direction
+                default:
+                    // no direction specified, so abort
+                    ClearText();
+                    break;
+            }
+
+            // If there's a locked door, unlocked it
+            if (mapArray[playerY + deltaY, playerX + deltaX] == TILE_LOCKED_DOOR)
+            {
+                // unlock the door
+                mapArray[playerY + deltaY, playerX + deltaX] = TILE_CLOSED_DOOR;
+
+                // Let the user know everything went well
+                Console.SetCursorPosition(2, MAP_HEIGHT + 4);
+                Console.WriteLine("You unlock the door with the key.");
+        
+
+            }
+
+            // lock an unlocked door
+            else if (mapArray[playerY + deltaY, playerX + deltaX] == TILE_CLOSED_DOOR)
+            {
+                // lock the door
+                mapArray[playerY + deltaY, playerX + deltaX] = TILE_LOCKED_DOOR;
+
+                // Let the user know everything went well
+                Console.SetCursorPosition(2, MAP_HEIGHT + 4);
+                Console.WriteLine("You lock the door with the key.");
+
+            }
+            // catch when the user tries to lock an open door, warn them
+            else if (mapArray[playerY + deltaY, playerX + deltaX] == TILE_OPEN_DOOR)
+            {
+                // warn the user
+                Console.SetCursorPosition(2, MAP_HEIGHT + 4);
+                Console.WriteLine("Try closing the door first");
+
+            }
+
+            // don't know what this tile type is
+            else
+            {
+                // Complain to the user
+                Console.SetCursorPosition(2, MAP_HEIGHT + 4);
+                Console.WriteLine("Nothing to use the key on!");
+            }
+        }
+
+    
+
+        public void ClearText()
+        {
+                        /** COMMANDS TO FIX INVENTORY TEXT ISSUES **/
+
+            Console.Clear();
+            DrawScreen(MAP_WIDTH, MAP_HEIGHT, mapArray);
+            // Draw the inventory
+            ShowInventory();
+
+            // Set the player's new position
+            Console.SetCursorPosition(playerX, playerY);
+
+            // Draw the player
+            Console.Write('@');
         }
 
         public void Main()
@@ -419,6 +670,11 @@ namespace ConsoleApplication1
                         break;
 
                     // Ignore other keys
+
+                    case ConsoleKey.U:
+                        // use item
+                        UseItemCommand();
+                        break;
                     default:
                         break;
                 }
